eb1e80fe5d167bb3cf372a8c5d706c2c83d8064d
Iiro Alhonen <iiro.alhonen@sanoma.com>
2022-08-16 14:46:12 +0300

    Refactor navigation bar items to use placement instead of hstack and spacer.

--- niftyinitiativetracker/niftyinitiativetracker/Components/ToolbarItems.swift
+++ niftyinitiativetracker/niftyinitiativetracker/Components/ToolbarItems.swift
@@ -18,34 +18,31 @@ struct ToolbarItems: ToolbarContent {
     @Binding var isAddingNewCreature: Bool
     
     var body: some ToolbarContent {
-        ToolbarItemGroup {
-            HStack {
-                Spacer()
-                if isEncounter {
-                    Button {
-                        encounterCharacters.sort {
-                            ($0.initiativeRoll + $0.modifier) > ($1.initiativeRoll + $1.modifier)
-                        }
-                    } label: {
-                        Image(systemName: "line.3.horizontal.decrease")
-                    }.disabled(encounterCharacters.isEmpty)
-                } else {
-                    Button {
-                        for index in 0..<characters.count {
-                            var character = characters[index]
-                            character.rerollInitiative()
-                            characters[index] = character
-                        }
-                        saveParty(characters)
-                    } label: {
-                        Image(systemName: "arrow.clockwise")
-                    }.disabled(characters.isEmpty)
-                }
+        ToolbarItemGroup(placement: .navigationBarTrailing) {
+            if isEncounter {
                 Button {
-                    isAddingNewCreature = true
+                    encounterCharacters.sort {
+                        ($0.initiativeRoll + $0.modifier) > ($1.initiativeRoll + $1.modifier)
+                    }
                 } label: {
-                    Image(systemName: "plus")
-                }
+                    Image(systemName: "line.3.horizontal.decrease")
+                }.disabled(encounterCharacters.isEmpty)
+            } else {
+                Button {
+                    for index in 0..<characters.count {
+                        var character = characters[index]
+                        character.rerollInitiative()
+                        characters[index] = character
+                    }
+                    saveParty(characters)
+                } label: {
+                    Image(systemName: "arrow.clockwise")
+                }.disabled(characters.isEmpty)
+            }
+            Button {
+                isAddingNewCreature = true
+            } label: {
+                Image(systemName: "plus")
             }
         }
     }
