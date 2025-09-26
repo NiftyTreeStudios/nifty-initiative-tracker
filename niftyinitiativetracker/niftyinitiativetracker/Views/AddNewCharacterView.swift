56a65af3cccee09aff799d8dd093b0cfda3b847d
Iiro Alhonen <iiro.alhonen@gmail.com>
2025-09-26 12:59:12 +0300

    Update Add Character design

--- niftyinitiativetracker/niftyinitiativetracker/Views/AddNewCharacterView.swift
+++ niftyinitiativetracker/niftyinitiativetracker/Views/AddNewCharacterView.swift
@@ -1,6 +1,6 @@
 //
-//  AddNewPCView.swift
-//  niftyinitiativetracker
+//  AddNewCharacterView.swift
+//  Nifty Initiative Tracker
 //
 //  Created by Iiro Alhonen on 20.2.2022.
 //
@@ -8,60 +8,65 @@
 import SwiftUI
 
 struct AddNewCharacterView: View {
-    
     @Binding var characters: [Character]
     @Binding var mobs: [Character]
     @Binding var isOpen: Bool
-    
+
     @State private var name: String = ""
     @State private var player: String = ""
     @State private var initiativeModifier: Int = 0
     @State var isPC: Bool
     @State var color: Color
-    
+
+    @Environment(\.dismiss) private var dismiss
+
+    private var isNameValid: Bool {
+        !name.isEmpty
+    }
+
     var body: some View {
-        VStack {
-            Text("Add new \(isPC ? "" : "N")PC")
-                .font(.title2)
+        NavigationView {
             Form {
                 Toggle("Player Character", isOn: $isPC)
                 TextField("Name", text: $name)
+                if !isNameValid {
+                    Text("Name is required")
+                        .font(.footnote)
+                        .foregroundColor(.red)
+                }
                 if isPC {
                     TextField("Player", text: $player)
                 }
                 Stepper("Modifier: \(initiativeModifier)", value: $initiativeModifier)
                 ColorPicker("Color", selection: $color)
             }
-            Button {
-                let newCharacter = Character(
-                    name: name,
-                    initiativeRoll: Int.random(in: 1...20),
-                    modifier: initiativeModifier,
-                    isPC: isPC,
-                    player: player,
-                    color: color
-                )
-                if isPC {
-                    if characters.firstIndex(of: newCharacter) == nil {
-                        characters.append(newCharacter)
+            .navigationTitle("Add Character")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save changes") {
+                        let newCharacter = Character(
+                            name: name,
+                            initiativeRoll: Int.random(in: 1...20),
+                            modifier: initiativeModifier,
+                            isPC: isPC,
+                            player: player.isEmpty ? nil : player,
+                            color: color
+                        )
+                        if isPC {
+                            if characters.firstIndex(of: newCharacter) == nil {
+                                characters.append(newCharacter)
+                            }
+                        } else {
+                            if mobs.firstIndex(of: newCharacter) == nil {
+                                mobs.append(newCharacter)
+                            }
+                        }
+                        isOpen = false
+                        dismiss()
                     }
-                } else if !isPC {
-                    if mobs.firstIndex(of: newCharacter) == nil {
-                        mobs.append(newCharacter)
-                    }
+                    .disabled(!isNameValid)
                 }
-                isOpen = false
-            } label: {
-                Text("Add new character")
-                    .foregroundColor(.white)
-                    .padding()
-                    .padding(.horizontal, 50)
-                    .background {
-                        Capsule()
-                            .foregroundColor(.blue)
-                    }
             }
-
-        }.padding(.vertical)
+        }
     }
 }
